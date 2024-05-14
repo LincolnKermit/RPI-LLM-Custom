ae9e5e10892f5003ddd539d7d5d93c2dfdfd3c82
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2024-05-14 13:08:51 +0200

    Update core.py

--- core.py
+++ core.py
@@ -1,46 +1,18 @@
-from TTS.api import TTS
+from gtts import gTTS
 import os, openai, pyaudio, torch
 import speech_recognition as sr
-# pip3 install pyttsx3 - for text to speech - Recommanded version pip3
-# Windows users might need to install "win32api"
-
-
-
-
-
-
-print("Initalising TTSCore...")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# List available 🐸TTS models
-print(TTS().list_models())
-
-# Init TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-# Model is approxitaely 2GB in size, so it might take some time to load...
-# 130 MB in lib folder, 1.8 GB in models folder
-
-
-
-
-
-
-
-# Run TTS
-# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-# Text to speech list of amplitude values as output
-wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
-
-
 
 
 openai_key = "none"
+
 
 # MAC OS : brew install portaudio
 # Ubuntu : sudo apt-get install python-pyaudio python3-pyaudio
 
 # Global
 # pip install pyaudio
+
+
 
 
 print("Initializing Speech Recognition...")
@@ -56,11 +28,21 @@ print("End of setup, ready to listen!")
 
 while True:
     with micro as source:
+        tts = gTTS('Hello, I am your personnal assistant. How can I help you?')
         print("Speak!")
         audio_data = r.listen(source)
         print("End!")
     result = r.recognize_google(audio_data)
     print (">", result)
+    tts = gTTS('Have you said, "' + result + '"?')
+    while result != "yes" or result != "exactly" or result != "True" or result != "of course":
+        with micro as source:
+            tts = gTTS('I am sorry that I am not able to understand you. Can you please repeat?')
+            print("Speak!")
+            audio_data = r.listen(source)
+            print("End!")
+        result = r.recognize_google(audio_data)
+
 
 
 # pip install openai
